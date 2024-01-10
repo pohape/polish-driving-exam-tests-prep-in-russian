31c03611ebd3d6c192adb9b320d4c2328ca7f5e5
Pavel Geveiler <pavelgeveiler@gmail.com>
2024-01-10 05:59:05 +0300

    server side bugfix

--- server_side/index.php
+++ server_side/index.php
@@ -8,7 +8,7 @@ $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 
 if (isset($input['text'])) {
-    echo json_encode(['translate' => Translate::translate($input['text'])]);
+    echo json_encode(Translate::translate($input['text']));
 } else {
     echo json_encode(['error' => 'No text provided']);
 }
