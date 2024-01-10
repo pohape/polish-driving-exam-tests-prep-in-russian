da9004374c9ef1bedc028fcefe127e08811e0575
Pavel Geveiler <pavelgeveiler@gmail.com>
2024-01-10 05:53:24 +0300

    server side

--- server_side/translate.class.php
+++ server_side/translate.class.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Translate {
     const API_URL = 'https://translate.api.cloud.yandex.net/translate/v2/translate';
     const API_KEY_FILE = 'api_key.txt';
@@ -9,26 +8,27 @@ class Translate {
     private static function translateViaApi($text) {
         $headers = [
             'Content-Type: application/json',
-            'Authorization: Api-Key ' . file_get_contents(__DIR__ . '/' . self::API_KEY_FILE)
+            'Authorization: Api-Key ' . trim(file_get_contents(__DIR__ . '/' . self::API_KEY_FILE))
         ];
-
+        
         $body = json_encode([
-            'texts' => [$text],
+            'texts' => ['TEST'],
             'sourceLanguageCode' => 'pl',
             'targetLanguageCode' => 'ru'
         ]);
-
+        
         $ch = curl_init(self::API_URL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-
+        
         $response = curl_exec($ch);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
+        
         curl_close($ch);
-        $decodedResponse = json_decode($response, true);
+	
+	$decodedResponse = json_decode($response, true);
 
         if ($httpCode == 200) {
             return array(
@@ -60,7 +60,7 @@ class Translate {
         } else {
             $translationResult = self::translateViaApi($text);
 
-            if ($translationResult['error'] === null) {
+            if ($translationResult['translate'] !== null) {
                 $cache[$text] = $translationResult['translate'];
                 file_put_contents($cachePath, json_encode($cache, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
             }
