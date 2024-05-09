1f62e41af84f95800621957dfeab7135e3aeb43d
Pavel Gev <pavelgeveiler@gmail.com>
2024-05-09 20:31:58 +0200

    Update sync_repo.sh

--- sync_repo.sh
+++ sync_repo.sh
@@ -3,7 +3,8 @@ script_dir=$(dirname "$0")
 cd "$script_dir"
 git pull
 chmod 777 translations.json
+chmod 777 favorites.json
 git add translations.json
-git commit -m "translations updated"
+git add favorites.json
+git commit -m "translations and favorites updated"
 git push
-
