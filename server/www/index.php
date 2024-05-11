2e269afc17610fe0e30805ee5e518c54cffad1b6
Pavel Gev <pavelgeveiler@gmail.com>
2024-05-11 20:45:02 +0200

    Try-catch

--- server/www/index.php
+++ server/www/index.php
@@ -21,94 +21,101 @@ function prepareText(string $text)
     return ['text' => $text, 'prefix' => $prefix];
 }
 
-$favorites = new Favorites();
-$translate = new Translate();
+try {
+    $favorites = new Favorites();
+    $translate = new Translate();
 
-if (isset($input['text'])) {
-    $prepared = prepareText($input['text']);
-    $result = $translate->performTranslation($prepared['text']);
+    if (isset($input['text'])) {
+        $prepared = prepareText($input['text']);
+        $result = $translate->performTranslation($prepared['text']);
 
-    if ($result['translate']) {
-        $result['translate'] = $prepared['prefix'] . $result['translate'];
-    }
-
-    echo json_encode($result, JSON_UNESCAPED_UNICODE);
-} elseif (isset($input['approve'])) {
-    $prepared = prepareText($input['approve']);
-
-    echo json_encode(
-        ['error' => null, 'success' => $translate->approveTranslation($prepared['text'])],
-        JSON_UNESCAPED_UNICODE
-    );
-} elseif (isset($input['mark_incorrect'])) {
-    $prepared = prepareText($input['mark_incorrect']);
-
-    echo json_encode(
-        ['error' => null, 'success' => $translate->markTranslationAsIncorrect($prepared['text'])],
-        JSON_UNESCAPED_UNICODE
-    );
-} elseif (isset($input['registration_date'])) {
-    if (isset($input['add_to_favorites'])) {
-        if (empty($input['question_id'])) {
-            echo json_encode(
-                [
-                    'error' => null,
-                    'success' => $favorites->saveToFavoritesByString(
-                        $input['add_to_favorites'],
-                        $input['registration_date']
-                    ),
-                    'favorites' => $favorites->getFavoritesShort($input['registration_date'])
-                ],
-                JSON_UNESCAPED_UNICODE
-            );
-        } else {
-            echo json_encode(
-                [
-                    'error' => null,
-                    'success' => $favorites->saveToFavoritesById(
-                        $input['add_to_favorites'],
-                        $input['registration_date']
-                    ),
-                    'favorites' => $favorites->getFavoritesShort($input['registration_date'])
-                ],
-                JSON_UNESCAPED_UNICODE
-            );
+        if ($result['translate']) {
+            $result['translate'] = $prepared['prefix'] . $result['translate'];
         }
-    } elseif (isset($input['remove_from_favorites'])) {
-        if (empty($input['question_id'])) {
-            echo json_encode(
-                [
-                    'error' => null,
-                    'success' => $favorites->removeFromFavoritesByString(
-                        $input['remove_from_favorites'],
-                        $input['registration_date']
-                    ),
-                    'favorites' => $favorites->getFavoritesShort($input['registration_date'])
-                ],
-                JSON_UNESCAPED_UNICODE
-            );
+
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    } elseif (isset($input['approve'])) {
+        $prepared = prepareText($input['approve']);
+
+        echo json_encode(
+            ['error' => null, 'success' => $translate->approveTranslation($prepared['text'])],
+            JSON_UNESCAPED_UNICODE
+        );
+    } elseif (isset($input['mark_incorrect'])) {
+        $prepared = prepareText($input['mark_incorrect']);
+
+        echo json_encode(
+            ['error' => null, 'success' => $translate->markTranslationAsIncorrect($prepared['text'])],
+            JSON_UNESCAPED_UNICODE
+        );
+    } elseif (isset($input['registration_date'])) {
+        if (isset($input['add_to_favorites'])) {
+            if (empty($input['question_id'])) {
+                echo json_encode(
+                    [
+                        'error' => null,
+                        'success' => $favorites->saveToFavoritesByString(
+                            $input['add_to_favorites'],
+                            $input['registration_date']
+                        ),
+                        'favorites' => $favorites->getFavoritesShort($input['registration_date'])
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            } else {
+                echo json_encode(
+                    [
+                        'error' => null,
+                        'success' => $favorites->saveToFavoritesById(
+                            $input['add_to_favorites'],
+                            $input['registration_date']
+                        ),
+                        'favorites' => $favorites->getFavoritesShort($input['registration_date'])
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            }
+        } elseif (isset($input['remove_from_favorites'])) {
+            if (empty($input['question_id'])) {
+                echo json_encode(
+                    [
+                        'error' => null,
+                        'success' => $favorites->removeFromFavoritesByString(
+                            $input['remove_from_favorites'],
+                            $input['registration_date']
+                        ),
+                        'favorites' => $favorites->getFavoritesShort($input['registration_date'])
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            } else {
+                echo json_encode(
+                    [
+                        'error' => null,
+                        'success' => $favorites->removeFromFavoritesById(
+                            intval($input['question_id']),
+                            $input['registration_date']
+                        ),
+                        'favorites' => $favorites->getFavoritesShort($input['registration_date'])
+                    ],
+                    JSON_UNESCAPED_UNICODE
+                );
+            }
         } else {
             echo json_encode(
-                [
-                    'error' => null,
-                    'success' => $favorites->removeFromFavoritesById(
-                        intval($input['question_id']),
-                        $input['registration_date']
-                    ),
-                    'favorites' => $favorites->getFavoritesShort($input['registration_date'])
-                ],
+                ['error' => null, 'favorites' => $favorites->getFavoritesShort($input['registration_date'])],
                 JSON_UNESCAPED_UNICODE
             );
         }
     } else {
         echo json_encode(
-            ['error' => null, 'favorites' => $favorites->getFavoritesShort($input['registration_date'])],
+            ['error' => null],
             JSON_UNESCAPED_UNICODE
         );
     }
-} else {
+} catch (Exception $e) {
     echo json_encode(
-        ['error' => null],
+        ['error' => $e->getMessage(), 'trace' => $e->getTraceAsString()],
         JSON_UNESCAPED_UNICODE
     );
 }
