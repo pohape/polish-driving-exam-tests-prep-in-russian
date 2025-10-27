6a77973c03890e9e77f9d2cc0b9eff5adb3f0da5
Pavel Gev <pavelgeveiler@gmail.com>
2025-10-27 09:30:11 +0100

    correct URL

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    const baseUrl = 'http://145.239.80.201:8080/'
+    const baseUrl = 'https://jazda.webscrapp.rocks/'
     const signsImagesBasePath = 'https://raw.githubusercontent.com/pohape/polish-driving-exam-tests-prep-in-russian/main/server/public/images/znaki/'
 
     const regexRegistrationDate = /Konto zostało utworzone: (.*?)<\/p>/;
